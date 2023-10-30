69ccb7f3e290805700faac7b054057d3d2995898
Kareem.Abouzaid <Kareem.abouzait@gmail.com>
2023-10-30 15:20:50 +0200

    last

--- Courses/models/res_partner.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-    
-    name=fields.Char(string="Name")
-    is_instructor=fields.Boolean(string="Is Instructor")
-    course=fields.Many2one('courses.course',string="Course")
-    course_id=fields.Many2one('courses.course', string='Course Id',)
